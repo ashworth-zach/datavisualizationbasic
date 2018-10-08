c79c8bf4b3131325407f33b9807d5af4059a5071
Zach Ashworth <zashworth2@gmail.com>
2018-10-08 08:02:49 -0500

    first commit

---
+++ leads/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, redirect, render_template, request, flash
+import pymysql.cursors
+import datetime
+
+# import the function connectToMySQL from the file mysqlconnection.py
+from mysqlconnection import connectToMySQL
+mysql = connectToMySQL("leads")
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    all_leads = mysql.query_db("SELECT * FROM users")
+    return render_template('index.html', leads = all_leads)
+# @app.route('/process', methods=['POST'])
+# def process():
+#     mysql = connectToMySQL("leads")
+#     query = "INSERT INTO leads (first_name, last_name, occupation, created_at, updated_at) VALUES (%(firstname)s, %(lastname)s, %(leads)s, NOW(), NOW());"
+#     firstname= request.form['firstname']
+#     lastname=  request.form['lastname']
+#     leads= request.form['leads']
+#     data={
+#         'firstname':firstname,
+#         'lastname':lastname,
+#         'leads':leads
+#     }
+#     new_friend_id = mysql.query_db(query, data)
+#     return redirect('/')
+if __name__ == "__main__":
+    app.run(debug=True)
